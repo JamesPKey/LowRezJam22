d4d77eecb52d0d9eceb615454fce46d20b7c2c31
James <james.key@gwplc.com>
2022-08-08 02:51:15 +0100

    Add basic Character movement

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,37 +1,53 @@
 import Phaser from 'phaser';
 
 export default class Game extends Phaser.Scene {
+  private keyW: any;
+  private keyA: any;
+  private keyS: any;
+  private keyD: any;
+  private keyE: any;
+
+  private playerX: any;
+  private playerY: any;
+
+  private player: any;
+
   constructor() {
     super('GameScene');
   }
 
-  preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
-  }
-
   create() {
-    const logo = this.add.image(32, 32, 'logo');
-    logo.setScale(0.15)
+    this.keyW = this.input.keyboard.addKey('W')
+    this.keyA = this.input.keyboard.addKey('A')
+    this.keyS = this.input.keyboard.addKey('S')
+    this.keyD = this.input.keyboard.addKey('D')
+    this.keyE = this.input.keyboard.addKey('E')
 
-    this.input.on('pointerup', (pointer: any) => {
-      this.scene.start('MenuScene');
-    }, this);
-
-
-    this.tweens.add({
-      targets: logo,
-      scaleX: 0.2,
-      scaleY: 0.2,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
-      repeat: -1
-    });
+    this.playerX = 32
+    this.playerY = 32
+    this.player = this.add.rectangle(this.playerX, this.playerY, 2, 2, 0xffffff)
 
   }
 
-  update(time: number, delta: number): void {
-    if (time > 2000)
-      this.scene.start("MenuScene")
+  update() {
+    if (this.keyW?.isDown) {
+      this.playerY = this.playerY - 1
+      this.player.setPosition(this.playerX, this.playerY)
+    }
+    if (this.keyA?.isDown) {
+      this.playerX = this.playerX - 1
+      this.player.setPosition(this.playerX, this.playerY)
+    }
+    if (this.keyS?.isDown) {
+      this.playerY = this.playerY + 1
+      this.player.setPosition(this.playerX, this.playerY)
+    }
+    if (this.keyD?.isDown) {
+      this.playerX = this.playerX + 1
+      this.player.setPosition(this.playerX, this.playerY)
+    }
+    if(this.keyE?.isDown) {
+      //Todo: interact
+    }
   }
 }
