c2ac4c8f92b50da51e24aa7e598f732208e2e2df
James <james.key@gwplc.com>
2022-08-13 04:26:18 +0100

    Add obstacles, and collision detection

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,5 +1,5 @@
 import Phaser from 'phaser';
-import { InputControls, textboxColour, whiteColour } from '../Common';
+import { blackColour, InputControls, textboxColour, whiteColour } from '../Common';
 
 const DEBUG_MODE = true // Disabled this before publish
 
@@ -7,6 +7,7 @@ export default class Game extends Phaser.Scene {
   private keys?: InputControls;
 
   private player!: Player;
+  private obstacles: Obstacle[] = []
 
   private activeTextBox?: TextBox;
 
@@ -17,22 +18,41 @@ export default class Game extends Phaser.Scene {
   create() {
     this.registerInputs()
 
+    this.obstacles.push(
+      new Obstacle(this, 8, 32, 16, 64),
+      new Obstacle(this, 48, 16, 8, 8),
+      new Obstacle(this, 48, 48, 12, 12),
+    )
+
     this.player = new Player(this, 32, 32)
+
   }
 
   update(time: any) {
     if (time % 3 != 0) {return}
     if (this.keys?.W.isDown && this.player.hitbox().top > 0) {
-      this.player.setY(this.player.y - 1)
+      const playerHitbox = this.player.hitbox(0, -1)
+      if (!this.obstacles.find(obstacle => obstacle.collides(playerHitbox))) {
+        this.player.setY(this.player.y - 1)
+      }
     }
     if (this.keys?.A.isDown && this.player.hitbox().left > 0) {
-      this.player.setX(this.player.x - 1)
+      const playerHitbox = this.player.hitbox(-1, 0)
+      if (!this.obstacles.find(obstacle => obstacle.collides(playerHitbox))) {
+        this.player.setX(this.player.x - 1)
+      }
     }
     if (this.keys?.S.isDown && this.player.hitbox().bottom < 64) {
-      this.player.setY(this.player.y + 1)
+      const playerHitbox = this.player.hitbox(0, 1)
+      if (!this.obstacles.find(obstacle => obstacle.collides(playerHitbox))) {
+        this.player.setY(this.player.y + 1)
+      }
     }
-    if (this.keys?.D.isDown  && this.player.hitbox().right < 64) {
-      this.player.setX(this.player.x + 1)
+    if (this.keys?.D.isDown && this.player.hitbox().right < 64) {
+      const playerHitbox = this.player.hitbox(1, 0)
+      if (!this.obstacles.find(obstacle => obstacle.collides(playerHitbox))) {
+        this.player.setX(this.player.x + 1)
+      }
     }
     if (this.keys?.E.isDown) {
       //Todo: interact
@@ -86,12 +106,12 @@ class Drawable {
   public h: number
   public drawable: any
 
-  constructor(scene: Phaser.Scene, x: number, y: number, w: number, h: number) {
+  constructor(scene: Phaser.Scene, x: number, y: number, w: number, h: number, colour: number) {
     this.x = x
     this.y = y
     this.w = w
     this.h = h
-    this.drawable = scene.add.rectangle(this.x, this.y, this.w, this.h, whiteColour)
+    this.drawable = scene.add.rectangle(this.x, this.y, this.w, this.h, colour)
   }
 
   setX = (x: number) => {
@@ -104,16 +124,31 @@ class Drawable {
     this.drawable.setPosition(this.x, this.y)
   }
 
-  hitbox = () => ({
-    top: this.y - (this.h/2),
-    left: this.x - (this.w/2),
-    bottom: this.y + (this.h/2),
-    right: this.x + (this.w/2)
+  hitbox = (offsetX: number = 0, offsetY: number = 0) => ({
+    top: (this.y + offsetY) - (this.h/2),
+    left: (this.x + offsetX) - (this.w/2),
+    bottom: (this.y + offsetY) + (this.h/2),
+    right: (this.x + offsetX) + (this.w/2)
   })
+
+  collides = (otherHitbox: any) => {
+    const hitbox = this.hitbox()
+    return hitbox.left < otherHitbox.right &&
+    hitbox.right > otherHitbox.left &&
+    hitbox.top < otherHitbox.bottom &&
+    hitbox.bottom > otherHitbox.top
+  }
 }
 
 class Player extends Drawable {
   constructor(scene: Phaser.Scene, x: number, y: number) {
-    super(scene, x, y, 2, 2)
+    super(scene, x, y, 2, 2, whiteColour)
   }
 }
+
+class Obstacle extends Drawable {
+  constructor(scene: Phaser.Scene, x: number, y: number, w: number, h: number) {
+    super(scene, x, y, w, h, blackColour)
+  }
+}
+
