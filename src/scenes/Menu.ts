eec0b95110e9100bc8e2236ecc461172a3877c9e
Joe Parker <guyman17@yahoo.com>
2022-08-15 08:34:57 +0100

    Music (#20)
    * Background music
    
    * Update src/scenes/Menu.ts
    
    Co-authored-by: Joe Parker <guyman17@yahoo.com>
    
    * Add menu, game music
    
    * Update credits
    
    Co-authored-by: James <james+github@jpkey.co.uk>

--- src/scenes/Menu.ts
+++ src/scenes/Menu.ts
@@ -3,14 +3,17 @@ import { gameFont, InputControls } from '../Common';
 
 export default class MenuScene extends Phaser.Scene {
   private keys?: InputControls;
-  private selection = 0
-  private menuIndicator: any
+  private selection = 0;
+  private menuIndicator: any;
+  private music!: Phaser.Sound.BaseSound;
 
   constructor() {
     super('MenuScene');
   }
 
-  preload() {}
+  preload() {
+    this.load.audio("menu-music", ["assets/audio/Menu.mp3"]);
+  }
 
   create() {
     const logo = this.add.text(32, 8, "Escape", {
@@ -40,6 +43,11 @@ export default class MenuScene extends Phaser.Scene {
     const helpText = this.add.text(32, 48, "Help", gameFont)
     helpText.setColor("#fff")
     helpText.setOrigin(0.5, 0.5)
+
+    this.music = this.sound.add("menu-music", { loop: true });
+    if (!this.sound.get("menu-music").isPlaying) {
+      this.music.play() 
+    }
   }
 
   update() {
@@ -51,13 +59,14 @@ export default class MenuScene extends Phaser.Scene {
       this.selection = 1
       this.menuIndicator.setPosition(12, 48)
     }
-    if(this.keys?.E.isDown) {
+    if (this.keys?.E.isDown) {
       if (this.selection == 0) {
+        this.music.stop();
+        this.sound.removeByKey("menu-music");
         this.scene.start('GameScene');
       } else {
         this.scene.start('HelpScene');
       }
-  
     }
   }
   
